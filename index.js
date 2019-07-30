71f6a0261bbd6cb2689b688ad81ac0617348d115
Peter <cirediatpl@gmail.com>
2019-07-29 20:59:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+var dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        console.log(e.key)
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+    if (left < 380) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
