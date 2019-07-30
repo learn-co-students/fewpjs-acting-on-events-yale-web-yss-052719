792b2dcb29ae40961ca5735490006910a1b5643d
Peter <cirediatpl@gmail.com>
2019-07-29 21:00:08 -0400

    Done.

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
     var leftNumbers = dodger.style.left.replace("px", "");
     var left = parseInt(leftNumbers, 10);
-    if (left < 380) {
+    if (left < 360) {
         dodger.style.left = `${left + 1}px`;
     }
 }
